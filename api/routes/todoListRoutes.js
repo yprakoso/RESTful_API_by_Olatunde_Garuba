5ed7dad6bb9defea204b45c940ac06656cb311c3
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-05-16 13:24:33 +0000

    add intercept for non-exist url

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -2,7 +2,6 @@
 module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
 
-
   // todoList Routes
   app.route('/tasks')
     .get(todoList.list_all_tasks)
@@ -13,4 +12,8 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+
+  app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
 };

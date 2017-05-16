7156e06ce79754eb1bbcf9429bcd889b184bbc54
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-05-16 03:48:15 +0000

    install bodyParser

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)

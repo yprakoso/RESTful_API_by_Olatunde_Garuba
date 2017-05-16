37e8318f6b6dbe1705f69b16689061016a0f01db
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-05-16 03:49:08 +0000

    register created routes in the server

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send('Something broke!')
-})
+});
 
 app.listen(port, function () {
   console.log('todo list RESTful API server started on: ' + port);
